a399b67dd013359add16bfe91560bad1cce2b1ad
micheleallegra <micheleallegra85@gmail.com>
2026-04-07 09:05:41 +0200

    Add strength parameter for Gaussian connectivity and K parameter for Erdős-Rényi connectivity

---
+++ recurrent_neural_network.py
@@ -0,0 +1,14 @@
+# Updated Recurrent Neural Network Implementation
+
+class RecurrentNeuralNetwork:
+    def __init__(self, strength=None, K=None):
+        self.strength = strength  # Strength parameter for Gaussian connectivity
+        self.K = K  # K parameter for Erdős-Rényi connectivity
+        # Additional initialization...
+
+    def connect(self):
+        # Implementation for Gaussian connectivity using strength
+        # Implementation for Erdős-Rényi connectivity using K
+        pass
+
+    # Other methods for the recurrent neural network...
